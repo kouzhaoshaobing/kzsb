f6c7f329344d772ddd333d9c383898c10abf060d
fakepoet <fakepoet0101@gmail.com>
2020-02-01 14:35:36 +0800

    [ADD] add template

--- server.py
+++ server.py
@@ -4,14 +4,27 @@ from flask import Flask
 from flask import request
 from flask.json import jsonify
 from utils import save_backup, load_backup
+from flask import render_template
 
 
 app = Flask(__name__)
 
 
+@app.route('/<string:route>')
+def shimo_handler(route):
+    doc_id = get_doc_id(route)
+    resp_data = get_content(doc_id)
+    return render_template('base.html', **resp_data)
+
+
 @app.route('/')
-def shimo_handler():
-    doc_id = request.args.get('docId', 'TXWgjPCDyXkrqWrX')
+def main_page():
+    doc_id = get_doc_id('')
+    resp_data = get_content(doc_id)
+    return render_template('base.html', **resp_data)
+
+
+def get_content(doc_id):
     r = requests.get('https://api.shimo.im/files/{}?html=true'.format(doc_id))
     r = json.loads(r.content)
     if not r:
@@ -19,8 +32,21 @@ def shimo_handler():
     save_backup(doc_id, r)
     name = r.get('name', '')
     content = r.get('content', {})
-    resp = dict(
+    updated = r.get('updated_at', '')
+    resp_data = dict(
         name=name,
-        content=content
+        content=content,
+        updated=updated
     )
-    return jsonify(resp)
+    return resp_data
+
+
+def get_doc_id(route):
+    mapping = {
+        'about': 'qkRQgTcHT6yrq6xQ',
+        'help': '8cKJRcwvcDGCD3tJ',
+        'qa': 'd6qDkhhGV3WVCWcW',
+        'manga': '9HrtJTWCP8xtr86g',
+        'friends': 'p6KrJhdWVTpvvJwV'
+    }
+    return mapping.get(route, 'Cqc3T3cxVGCpDtJQ')
